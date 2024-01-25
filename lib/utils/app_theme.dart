1cd2c2bf262c2cf16a6930a5b346da5488d961da
MohammedGhazaly <warendragon67@gmail.com>
2024-01-25 11:01:44 +0200

    Added app theme

---
+++ lib/utils/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:grocer_books/utils/app_colors.dart';
+import 'package:grocer_books/utils/app_constants.dart';
+
+class AppTheme {
+  static ThemeData mainTheme = ThemeData(
+    fontFamily: kFontFamily,
+    primaryColor: AppColors.primaryColor,
+    primaryColorLight: AppColors.primaryColorLight,
+    useMaterial3: true,
+  );
+}
